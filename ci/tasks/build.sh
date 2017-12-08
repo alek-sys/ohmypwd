2a4bde260ee8c1f91e98b910a3a894d9970353e8
Alexey Nesterov <alexey.nesterov@live.com>
2017-12-08 22:15:09 +0000

    Fix path to Dockerfile

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,9 +2,12 @@
 
 set -e -u -x
 
-cd sources/
+export ROOT_FOLDER=$(pwd)
+export GRADLE_USER_HOME="${ROOT_FOLDER}/.gradle"
+
+cd ohmypwd/
 
 ./gradlew build
 
-cp build/libs/ohmypwd.jar build/
-cp Dockerfile build/
+cp ./build/libs/ohmypwd.jar build/
+cp ./ci/tasks/Dockerfile build/
